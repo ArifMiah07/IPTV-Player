98a7a66d92c2b9938644d80d55ba4d882c6e65f3
ArifMiah07 <arifmiah.me322@gmail.com>
2025-10-26 20:56:31 +0600

    project setup, git init, installed tailwind css v4, react-router-dom

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { invoke } from "@tauri-apps/api/core";
+import "./App.css";
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  return (
+    <main className="container">
+      <h1 className="text-red-400 font-bold text-7xl">IPTV-Player</h1>
+    </main>
+  );
+}
+
+export default App;
