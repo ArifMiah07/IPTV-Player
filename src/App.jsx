f52667dc570f028ed85db79cd9cb2088170cf474
ArifMiah07 <arifmiah.me322@gmail.com>
2025-10-27 03:45:59 +0600

    connected with server and testing API from backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,77 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
-import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-	// functions
-	const a = 0;
-	let b;
-	const CS = 23020;
+  // state
+  const [channelsData, setChannelsData] = useState(null);
+  const [channelsLength, setChannelsLength] = useState(0);
+  //
+  const [streamsData, setStreamsData] = useState(null);
+  const [streamsLength, setStreamsLength] = useState(0);
+  //
+  const [feedsData, setFeedsData] = useState(null);
+  const [feedsLength, setFeedsLength] = useState(0);
+  //
+  useEffect(() => {
+    const getChannelsData = async () => {
+      try {
+        const res = await axios.get(
+          "http://localhost:5000/api/iptv-player/channels"
+        );
+        setChannelsData(res?.data?.data);
+        setChannelsLength(res?.data?.length);
+      } catch (error) {
+        console.log(error);
+        throw new Error("Something went wrong!");
+      }
+    };
+    getChannelsData();
+  }, []);
+  //
+  useEffect(() => {
+    const getStreamsData = async () => {
+      try {
+        const res = await axios.get(
+          "http://localhost:5000/api/iptv-player/streams"
+        );
+        setStreamsData(res?.data?.data);
+        setStreamsLength(res?.data?.length);
+      } catch (error) {
+        console.log(error);
+        throw new Error("Something went wrong!");
+      }
+    };
+    getStreamsData();
+  }, []);
+  //
+  useEffect(() => {
+    const getFeedsData = async () => {
+      try {
+        const res = await axios.get(
+          "http://localhost:5000/api/iptv-player/feeds"
+        );
+        setFeedsData(res?.data?.data);
+        setFeedsLength(res?.data?.length);
+      } catch (error) {
+        console.log(error);
+        throw new Error("Something went wrong!");
+      }
+    };
+    getFeedsData();
+  }, []);
 
-	return (
-		<main className="container">
-			<h1 className="text-red-400 font-bold text-7xl">IPTV-Player</h1>
-		</main>
-	);
+  // functions
+  console.log(channelsData);
+  return (
+    <main className="container">
+      <h1 className="text-[#ff0062] text-shadow-md font-bold text-7xl ">
+        IPTV Player
+      </h1>
+      <h1>total channels : {channelsLength}</h1>
+      <h1>total streams : {streamsLength}</h1>
+      <h1>total feeds : {feedsLength}</h1>
+    </main>
+  );
 }
 
 export default App;
