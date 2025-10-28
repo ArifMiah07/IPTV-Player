ce625b261a502e38887b497a2b9471bbae65e336
ArifMiah07 <arifmiah.me322@gmail.com>
2025-10-28 23:49:17 +0600

    working on landin page, like css, like cpp

---
+++ src/components/landing/LandingPageFrame.jsx
@@ -0,0 +1,11 @@
+export default function LandingPageFrame() {
+  return (
+    <section className=" mx-auto min-w-screen min-h-screen relative z-0 overflow-hidden before:content-[''] before:absolute before:inset-0 before:bg-[radial-gradient(#ff00ff_0%,#ff00ff_50%,#ffffff_100%)] before:opacity-10 before:-z-10    ">
+      <div className="bg-red-50">
+        <h1 className="z-10 text-7xl text-black">
+          this is landing page frame section
+        </h1>
+      </div>
+    </section>
+  );
+}
--- src/pages/landing/LandingPage.jsx
+++ src/pages/landing/LandingPage.jsx
@@ -1,68 +1,84 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import bannerImage from "../../assets/banner.png";
-import bannerImageStatic from "../../assets/banner-static.png";
+import LandingPageFrame from "../../components/landing/LandingPageFrame";
+
 export default function LandingPage() {
-  // state
-  const [channelsData, setChannelsData] = useState(null);
-  const [channelsLength, setChannelsLength] = useState(0);
-  //
-  const [streamsData, setStreamsData] = useState(null);
-  const [streamsLength, setStreamsLength] = useState(0);
-  //
-  const [feedsData, setFeedsData] = useState(null);
-  const [feedsLength, setFeedsLength] = useState(0);
-  //
-  useEffect(() => {
-    const getChannelsData = async () => {
-      try {
-        const res = await axios.get(
-          "http://localhost:5000/api/iptv-player/channels"
-        );
-        setChannelsData(res?.data?.data);
-        setChannelsLength(res?.data?.length);
-      } catch (error) {
-        console.log(error);
-        throw new Error("Something went wrong!");
-      }
-    };
-    getChannelsData();
-  }, []);
-  //
-  useEffect(() => {
-    const getStreamsData = async () => {
-      try {
-        const res = await axios.get(
-          "http://localhost:5000/api/iptv-player/streams"
-        );
-        setStreamsData(res?.data?.data);
-        setStreamsLength(res?.data?.length);
-      } catch (error) {
-        console.log(error);
-        throw new Error("Something went wrong!");
-      }
-    };
-    getStreamsData();
-  }, []);
-  //
-  useEffect(() => {
-    const getFeedsData = async () => {
-      try {
-        const res = await axios.get(
-          "http://localhost:5000/api/iptv-player/feeds"
-        );
-        setFeedsData(res?.data?.data);
-        setFeedsLength(res?.data?.length);
-      } catch (error) {
-        console.log(error);
-        throw new Error("Something went wrong!");
-      }
-    };
-    getFeedsData();
-  }, []);
   return (
-    <div>
-      this is landing page
+    <div className=" box-border mx-auto w-full h-full ">
+      <LandingPageFrame />
+    </div>
+  );
+}
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * // import axios from "axios";
+ *  // import { useEffect, useState } from "react";
+ *  import bannerImage from "../../assets/banner.png";
+ * import bannerImageStatic from "../../assets/banner-static.png";
+ * 
+ * 
+ * // // state
+  // const [channelsData, setChannelsData] = useState(null);
+  // const [channelsLength, setChannelsLength] = useState(0);
+  // //
+  // const [streamsData, setStreamsData] = useState(null);
+  // const [streamsLength, setStreamsLength] = useState(0);
+  // //
+  // const [feedsData, setFeedsData] = useState(null);
+  // const [feedsLength, setFeedsLength] = useState(0);
+  // //
+  // useEffect(() => {
+  //   const getChannelsData = async () => {
+  //     try {
+  //       const res = await axios.get(
+  //         "http://localhost:5000/api/iptv-player/channels"
+  //       );
+  //       setChannelsData(res?.data?.data);
+  //       setChannelsLength(res?.data?.length);
+  //     } catch (error) {
+  //       console.log(error);
+  //       throw new Error("Something went wrong!");
+  //     }
+  //   };
+  //   getChannelsData();
+  // }, []);
+  // //
+  // useEffect(() => {
+  //   const getStreamsData = async () => {
+  //     try {
+  //       const res = await axios.get(
+  //         "http://localhost:5000/api/iptv-player/streams"
+  //       );
+  //       setStreamsData(res?.data?.data);
+  //       setStreamsLength(res?.data?.length);
+  //     } catch (error) {
+  //       console.log(error);
+  //       throw new Error("Something went wrong!");
+  //     }
+  //   };
+  //   getStreamsData();
+  // }, []);
+  // //
+  // useEffect(() => {
+  //   const getFeedsData = async () => {
+  //     try {
+  //       const res = await axios.get(
+  //         "http://localhost:5000/api/iptv-player/feeds"
+  //       );
+  //       setFeedsData(res?.data?.data);
+  //       setFeedsLength(res?.data?.length);
+  //     } catch (error) {
+  //       console.log(error);
+  //       throw new Error("Something went wrong!");
+  //     }
+  //   };
+  //   getFeedsData();
+  // }, []);
+ * 
+ * 
+ * this is landing page
       <h1 className="text-[#ff0062] text-shadow-md font-bold text-7xl ">
         IPTV Player
       </h1>
@@ -81,6 +97,4 @@ export default function LandingPage() {
           alt="banner image"
         />
       </div>
-    </div>
-  );
-}
+*/
