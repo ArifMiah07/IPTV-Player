ce68cfc9fce7480fd9471b2c1065a94b359b3ca8
ArifMiah07 <arifmiah.me322@gmail.com>
2025-10-30 00:36:57 +0600

    working on home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,20 +2,33 @@ import { Outlet } from "react-router-dom";
 
 export default function HomePage() {
   return (
-    <section className="border border-red-500  ">
+    <section className="relative w-full h-full box-border  ">
       {/* header */}
-      <header className="border-2 border-green-500">
-        <nav>This is global nav bar</nav>
+      {/* glowing layer only positioned under header */}
+      <div className="absolute top-0 left-0 w-full h-[60px] bg-[#00B3FF] blur-[300px] opacity-100 -z-10"></div>
+
+      <header className="relative z-10 w-full  h-[60px]  lg:px-4 py-4">
+        <nav className="fixed">This is global nav bar</nav>
       </header>
       {/* main content */}
-      <main className=" grid grid-cols-12 border-4 border-blue-500">
+      <main className=" relative w-full h-full min-h-[calc(200vh-60px)] grid grid-cols-12 ">
+        <div className="absolute top-0 left-0 w-full h-full bg-[radial-gradient(#ff00ff_0%,#ff00ff_50%,#ffffff_100%)] blur-[300px] opacity-10 -z-10"></div>
         {/* sidebar */}
-        <section className="col-span-1 border border-purple-500 ">
+        <section className="col-span-1  ">
           <div>side</div>
         </section>
+        {/* content */}
         <section className="col-span-11">
+          <p>
+            this is main content Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Consectetur est nulla quos doloribus ea?
+            Doloremque magnam molestiae, officiis quis praesentium possimus
+            debitis asperiores accusantium at sit deleniti provident quisquam,
+            itaque velit pariatur neque nulla, perferendis fuga necessitatibus
+            magni? Sunt animi facere possimus suscipit quia officiis quis
+            temporibus voluptas consequatur vero?
+          </p>
           <p>this is main content</p>
-          <Outlet />
         </section>
       </main>
     </section>
