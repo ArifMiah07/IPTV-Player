07266da956b8995a36574bc3ed2bf10a24b26bcd
ArifMiah07 <arifmiah.me322@gmail.com>
2025-10-30 07:01:54 +0600

    add motion for animation

--- src/components/features/WatchLiveStreams.jsx
+++ src/components/features/WatchLiveStreams.jsx
@@ -2,7 +2,7 @@ export default function WatchLiveStreams(){
     return(
         <section className="flex flex-col w-full h-full ">
             {/* heading */}
-            <h1>Watch live Streams</h1>
+            <h1  className="text-[#000000b3] text-[24px] font-bold  " >Watch live Streams</h1>
             {/* content */}
             <div>
                 {/*  */}
--- src/pages/home/features/Features.jsx
+++ src/pages/home/features/Features.jsx
@@ -1,17 +1,41 @@
+import { useEffect, useState } from "react";
+
 import PlayAndDownloadVideo from "../../../components/features/PlayAndDownloadVideo";
 import WatchLiveStreams from "../../../components/features/WatchLiveStreams";
+import { motion, AnimatePresence } from "motion/react";
 
 export default function FeaturesPage() {
+  const [showWelcome, setShowWelcome] = useState(true);
+
+  // Hide the welcome message after 3 seconds
+  useEffect(() => {
+    const timer = setTimeout(() => setShowWelcome(false), 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <section className="w-full h-full flex flex-col gap-5 ">
+    <section className="w-full h-full flex flex-col gap-2.5 ">
       {/* welcome message */}
-      <div className="w-full h-[54px]   bg-[#D9D9D933] p-5 backdrop-blur-xs ">
-        <h1>Welcome To IPTV Player</h1>
-      </div>
-     <div className="w-full h-[300px]   bg-[#D9D9D933] p-5 backdrop-blur-xs ">
+      <AnimatePresence>
+        {showWelcome && (
+          <motion.div
+            key="welcome"
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -10 }}
+            transition={{ duration: 0.8 }}
+            className="w-full h-[54px] bg-[#D9D9D933] p-5 backdrop-blur-xs flex ">
+            <h1 className="text-[#000000B3] text-[16px] font-semibold">
+              Welcome To IPTV Player
+            </h1>
+          </motion.div>
+        )}
+      </AnimatePresence>
+      {/* features */}
+      <div className="w-full h-[300px]   bg-[#D9D9D933] p-5 backdrop-blur-xs ">
         <WatchLiveStreams></WatchLiveStreams>
       </div>
-     <div className="w-full h-[300px]   bg-[#D9D9D933] p-5 backdrop-blur-xs ">
+      <div className="w-full h-[300px]   bg-[#D9D9D933] p-5 backdrop-blur-xs ">
         <PlayAndDownloadVideo></PlayAndDownloadVideo>{" "}
       </div>
     </section>
