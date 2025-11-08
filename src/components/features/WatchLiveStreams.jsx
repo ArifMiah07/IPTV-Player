b4397bfc8e3f1582bdd612d22c14508b74514b98
ArifMiah07 <arifmiah.me322@gmail.com>
2025-11-08 14:07:16 +0600

    added tooltip

--- src/components/features/WatchLiveStreams.jsx
+++ src/components/features/WatchLiveStreams.jsx
@@ -1,21 +1,39 @@
+import { useState } from "react";
 import { NavMenuButton } from "../buttons/CustomButtons";
 
-export default function WatchLiveStreams(){
-    return(
-        <section className="flex flex-col w-full h-full ">
-            {/* heading */}
-            <h1  className="text-[#000000b3] text-[24px] font-bold  " >Watch Live Streams</h1>
-            {/* content */}
-            <div>
-                {/*  */}
-                <div>
-                    <NavMenuButton label={"Lunch IPTV Now"} ></NavMenuButton>
-                </div>
-                {/*  */}
-                <div>
+const [isSectionClose, setIsSectionClose] = useState(false);
 
-                </div>
-            </div>
-        </section>
-    )
+console.log(isSectionClose);
+const handleSectionVisibility = () => {
+  console.log("clicked");
+  setIsSectionClose(!isSectionClose);
+  console.log(isSectionClose);
+};
+
+export default function WatchLiveStreams() {
+  return (
+    <section className=" relative flex flex-col w-full h-full ">
+      <div className="absolute top-0 right-0">
+        <button
+          onClick={handleSectionVisibility}
+          className="cursor-pointer tooltip ">
+          <span className="tooltiptext text-sm">Hide</span>X
+        </button>
+      </div>
+      {/* heading */}
+      <h1 className="text-[#000000b3] text-[24px] font-bold  ">
+        Watch Live Streams
+      </h1>
+      <span>{isSectionClose ? "hide" : "close"}</span>
+      {/* content */}
+      <div>
+        {/*  */}
+        <div>
+          <NavMenuButton label={"Lunch IPTV Now"}></NavMenuButton>
+        </div>
+        {/*  */}
+        <div></div>
+      </div>
+    </section>
+  );
 }
