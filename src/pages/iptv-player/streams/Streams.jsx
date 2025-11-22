3dd65a515f27a0b64f20567f013f2b45868b83ee
ArifMiah07 <arifmiah.me322@gmail.com>
2025-11-22 22:31:56 +0600

    update

--- src/pages/iptv-player/streams/Streams.jsx
+++ src/pages/iptv-player/streams/Streams.jsx
@@ -1,13 +1,61 @@
-import ReactPlayer from "react-player";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
+//
 export default function StreamPage() {
+  // react states
+  const [streams, setStreams] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  // react side effects
+  // fetch streams api from backend
+  useEffect(() => {
+    // fetch
+    async function fetchStreams() {
+      try {
+        const streams_api_url = "http://localhost:5000/api/iptv-player/streams";
+        const response = await axios.get(streams_api_url);
+        console.log({ response });
+        setStreams(response?.data?.data);
+        setError(null); // clear previous error
+      } catch (error) {
+        //
+        console.log(error);
+        setError(error.message || "something went wrong!");
+      } finally {
+        //
+        setLoading(false);
+        console.log("complete");
+      }
+    }
+    // call
+    fetchStreams();
+  }, []);
+
+  if(loading) return <div>loading steams....</div>
+  if(error) return <div>Error:....{error}</div>
+
+  //
   return (
-    <div>
+    // streams page component
+    <div className="w-full h-full">
       <h1>this is IPTV Streaming page</h1>
-      <div className=" border border-red-500 flex flex-row gap-3 p-3 w-[700px]">
+      <div className=" border border-red-500 flex flex-row gap-3 p-3 w-full">
         <div className="border border-green-500  w-[200px] h-[200px]"></div>
         <div className="border border-green-500  w-[200px] h-[200px]"></div>
         <div className="border border-green-500  w-[200px] h-[200px]"></div>
+      </div>
+      <div>
+        <ul>
+          {
+            streams.slice(0, 10).map((stream_item, stream_index)=>  <li className=" w-full h-full pa-2 border border-red-500"  key={stream_index}>
+              <p>Stream index: {stream_index}</p>
+              <p>Stream title: {stream_item.title}</p>
+              <p className=" w-full h-full flex flex-col items-start justify-start break-all">Stream url: {stream_item.url}</p>
+            </li> )
+          }
+        </ul>
       </div>
     </div>
   );
