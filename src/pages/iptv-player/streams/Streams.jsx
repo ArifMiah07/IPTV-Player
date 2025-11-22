fbf057d70154fd6c0b7d71c85a5059810c9be6a7
ArifMiah07 <arifmiah.me322@gmail.com>
2025-11-22 21:43:29 +0600

    update

--- src/pages/iptv-player/streams/Streams.jsx
+++ src/pages/iptv-player/streams/Streams.jsx
@@ -4,8 +4,10 @@ export default function StreamPage() {
   return (
     <div>
       <h1>this is IPTV Streaming page</h1>
-      <div className="w-[700px]">
-        <ReactPlayer crossOrigin="use-credentials" src="http://cfd-v4-service-channel-stitcher-use1-1.prd.pluto.tv/stitch/hls/channel/65a9b20f0c7ff50008d3a3b6/master.m3u8?appName=w…eviceType=web&deviceVersion=unknown&includeExtendedEvents=false&serverSideAds=false&sid=7d2a2bb4-c275-4221-a998-d1a0bf52b786" />
+      <div className=" border border-red-500 flex flex-row gap-3 p-3 w-[700px]">
+        <div className="border border-green-500  w-[200px] h-[200px]"></div>
+        <div className="border border-green-500  w-[200px] h-[200px]"></div>
+        <div className="border border-green-500  w-[200px] h-[200px]"></div>
       </div>
     </div>
   );
